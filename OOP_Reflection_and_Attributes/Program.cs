ddd6b220faf7d91c32622878c77a16bcc5326195
Emil Hristov <emodavitkov@abv.bg>
2021-07-10 23:02:15 +0300

    Go GO

---
+++ OOP_Reflection_and_Attributes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_Reflection_and_Attributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
